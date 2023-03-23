4bdb9d1e3da4b0e8064aefceb6c539447c6df30e
jaebrn <80350744+jaebrn@users.noreply.github.com>
2023-03-23 19:15:36 -0400

    More comments and cleanup

--- template-p5-project/js/test.js
+++ template-p5-project/js/test.js
@@ -1,24 +1,24 @@
-let balloon = [];
+let balloon = []; // array storing balloon sprites
 let balloonCount = 3;
 
 let lives = 3;
-let heart;
-let heartW = 100;
+let heart; // heart/life sprite
+let heartW = 100; // width of heart sprite
 let score = 0;
 
-let diameter = 150;
+let diameter = 150; // balloon diameter
 
 function preload() {
     heart = loadImage('assets/heart.png');
 }
 
 function setup() {
-    createCanvas(1000, 1000);
+    createCanvas(windowWidth, windowHeight);
     background(100);
 
     world.gravity.y = 2;
 
-    for (i = 0; i < balloonCount; i++) {
+    for (i = 0; i < balloonCount; i++) { // initial array population & balloon spawning
         balloon[i] = new Sprite();
         balloon[i].diameter = 150;
         balloon[i].y = random(-balloon[i].diameter / 2, -balloon[i].diameter);
@@ -29,14 +29,14 @@ function setup() {
 function draw() {
     background(100);
 
-    for (i = 0; i < balloon.length; i++) {
-        if (balloon[i].y > height + balloon[i].diameter) {
+    for (i = 0; i < balloon.length; i++) { //manages active balloon sprite
+        if (balloon[i].y > height + balloon[i].diameter) { // sprites deleted if off the screen 
             balloon.splice(i, 1);
             lives--;
         }
     }
 
-    if (balloon.length < balloonCount) {
+    if (balloon.length < balloonCount) { //respawns balloons if array isn't fully populated
         respawn();
     }
 
@@ -51,12 +51,12 @@ function printScore() { // prints the current score on screen
     text(score, width - 70, 100);
 }
 
-function printLives() {
+function printLives() { // prints the current number of lives on screen 
     for (i = 0; i < lives; i++) {
         image(heart, heartW * i + 10, 0, heartW, heartW);
     }
 }
 
-function respawn() {
+function respawn() { // adds a new balloon sprite to the array
     balloon.push(new Sprite(random(diameter / 2, width - diameter / 2), random(-diameter / 2, -diameter / 2), 150));
 }
