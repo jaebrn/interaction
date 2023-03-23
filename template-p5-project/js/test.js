c6baa9e6367bf70b26bd1b93a703869a3c06618b
jaebrn <80350744+jaebrn@users.noreply.github.com>
2023-03-23 19:39:44 -0400

    Walls added + variation in x velocity

--- template-p5-project/js/test.js
+++ template-p5-project/js/test.js
@@ -1,12 +1,16 @@
 let balloon = []; // array storing balloon sprites
 let balloonCount = 3;
+let diameter = 150; // balloon diameter
+
+let leftWall, rightWall; // sprite collision walls;
 
 let lives = 3;
 let heart; // heart/life sprite
 let heartW = 100; // width of heart sprite
+
 let score = 0;
 
-let diameter = 150; // balloon diameter
+
 
 function preload() {
     heart = loadImage('assets/heart.png');
@@ -24,6 +28,20 @@ function setup() {
         balloon[i].y = random(-balloon[i].diameter / 2, -balloon[i].diameter);
         balloon[i].x = random(balloon[i].diameter / 2, width - balloon[i].diameter / 2);
     }
+
+    rightWall = new Sprite();
+    rightWall.x = width + 10;
+    rightWall.h = height;
+    rightWall.y = rightWall.h / 2;
+    rightWall.w = 10;
+    rightWall.collider = 'static';
+
+    leftWall = new Sprite();
+    leftWall.x = -10;
+    leftWall.h = height;
+    leftWall.w = 10;
+    leftWall.collider = 'static';
+
 }
 
 function draw() {
@@ -49,6 +67,7 @@ function mouseClicked() { // registers when the balloon is being clicked on
     for (i = 0; i < balloonCount; i++) {
         if (dist(mouseX, mouseY, balloon[i].x, balloon[i].y) < balloon[i].diameter / 2) { // if clicking balloon
             balloon[i].vel.y = -5; // will alter this to make it directionally accurate once motion controls are added
+            balloon[i].vel.x = random(-5, 5);
             print('HIT');
         }
     }
